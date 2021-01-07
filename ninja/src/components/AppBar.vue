0cb6118943d2cfaa388d56e26526ef59210e8a04
JuliaBurri <julia.burri@web.de>
2021-01-07 15:10:39 +0100

    changed tabs to navigationDrawer

--- ninja/src/components/AppBar.vue
+++ ninja/src/components/AppBar.vue
@@ -1,29 +1,55 @@
 <template>
-    <v-app-bar
-            color="#666666"
-            dark
-            height="100"
-    >
-        <router-link to="/" class="ml-2">
-            <v-img class="logo mt-2" contain
-                   :src="require('@/assets/ninjaBlack.png')"
-                   max-width="60">
-            </v-img>
-        </router-link>
-        <template>
-            <v-tabs centered>
-                <v-tab to="/">HOME</v-tab>
-                <v-tab to="/game">GAME</v-tab>
-                <v-tab to="/highscore">HIGHSCORE</v-tab>
-                <v-tab to="/about">ABOUT</v-tab>
-            </v-tabs>
-        </template>
-    </v-app-bar>
+    <div>
+        <v-navigation-drawer v-model="drawer" temporary fixed app>
+            <v-list nav>
+                <v-list-item-group
+                        v-model="group">
+                    <v-list-item class="drawer-buttons"
+                                 v-for="item in this.tabs"
+                                 :key="item.name" link :to="item.route">
+                            <v-list-item-icon>
+                                <v-icon>{{item.icon}}</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-title v-text="item.name"></v-list-item-title>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar
+                color="#666666"
+                dark
+                height="80"
+        >
+            <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+            <router-link to="/" class="ml-2">
+                <v-img class="logo mt-2" contain
+                       :src="require('@/assets/ninjaBlack.png')"
+                       max-width="60">
+                </v-img>
+            </router-link>
+            <v-spacer></v-spacer>
+            <v-app-bar-title>NINJA</v-app-bar-title>
+        </v-app-bar>
+    </div>
+
 </template>
 
 <script>
     export default {
-        name: "AppBar"
+        name: "AppBar",
+        data: () => ({
+            drawer: false,
+            group: null,
+            tabs : [
+                {name: "Home", route: '/', icon: 'mdi-home'},
+                {name: "Game", route: "/game", icon: 'mdi-play-circle'},
+                {name: "Highscore", route: "/highscore", icon: 'mdi-clipboard-list'},
+                {name: "About", route: "/about", icon: 'mdi-message-processing'},
+            ],
+        }),
+
     }
 </script>
 
