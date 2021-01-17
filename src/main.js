4a1322ed9c95f2a1b2a896b7d45d762090a1cfd2
JuliaBurri <julia.burri@web.de>
2021-01-17 12:54:59 +0100

    back to wss

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import './registerServiceWorker'
 Vue.use(VueResource)
 
 Vue.use(VueRouter);
-Vue.use(VueNativeSock, ('https://ninja-backend.herokuapp.com/websocket').replace('https', 'wss'), {
+Vue.use(VueNativeSock, ('wss://ninja-backend.herokuapp.com/websocket'), {
   reconnection: true,
   reconnectionAttempts: Infinity,
   reconnectionDelay: 10,
